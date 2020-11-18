265b801cb4a2ec5fe72b76ab136b304659a81f47
zehra1361 <zehra1361>
2020-11-18 17:42:21 +0200

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,6 +7,6 @@ public class BirinciGun {
                         System.out.println("10:11 -ikinci Gun");
                         System.out.println("10:26 -ikinci Gun");        
                         System.out.println("10:35 -ikinci gun"); 
-                        
+                        System.out.println("11:05 -local");
             }        
             }                        
