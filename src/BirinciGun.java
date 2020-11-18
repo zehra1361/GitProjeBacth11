994bb44699e766f43011ae4de292a9e9f8c84046
zehra1361 <zehra1361>
2020-11-18 17:54:22 +0200

    conflict cozuldu

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,11 +7,8 @@ public class BirinciGun {
                         System.out.println("10:11 -ikinci Gun");
                         System.out.println("10:26 -ikinci Gun");        
                         System.out.println("10:35 -ikinci gun"); 
-<<<<<<< HEAD
                         System.out.println("11:05 -local");
-=======
-                        System.out.println("11:03 -github.com");
                         
->>>>>>> cf613f261750865536467cd0d4eefeb023e49149
+                     
             }        
             }                        
