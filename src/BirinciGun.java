7ac8bdcf7c10ce66b0065ac1af495073364e8496
zehra1361 <74564662+zehra1361@users.noreply.github.com>
2020-11-18 17:30:27 +0200

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
             	System.out.println("ikinci commit");
                         System.out.println("10:11 -ikinci Gun");
                         System.out.println("10:26 -ikinci Gun");        
-                        System.out.println("10:35 -ikinci gun");        
+                        System.out.println("10:35 -ikinci gun"); 
+                        
             }        
             }                        
