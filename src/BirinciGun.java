13f28d2dc73ed872231d3faa0cc63517137125b8
zehra1361 <zehra1361>
2020-11-18 13:27:54 +0200

    ucuncu kommit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,7 +1,7 @@
 
 public class BirinciGun {
             public static void main(String[]args) {
-            	System.out.println("11:05 - Birinci Gun");
+            	
             	System.out.println("11:57 -Birinci Gun");
             	System.out.println("ikinci commit");
             }
