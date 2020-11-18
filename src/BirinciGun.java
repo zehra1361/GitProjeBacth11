9b846b8e98914d676646028eb1ad1bddb360ed75
zehra1361 <74564662+zehra1361@users.noreply.github.com>
2020-11-18 20:30:19 +0200

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -10,7 +10,7 @@ public class BirinciGun {
                         System.out.println("11:05 -local");
                         System.out.println("11:23 -github.com");
                         System.out.println("ben burada conflict beklemiyorum..");
-
+                        System.out.println("yepyeni bir satir");
                         
 
 
